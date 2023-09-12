ed2be1dbb02d3bdbbf3db426ba89758cb0cf4ee6
이창현 <116571873+LeeChangHyun48@users.noreply.github.com>
2023-09-12 20:48:15 +0900

    [Silver I] Title: 경로 찾기, Time: 428 ms, Memory: 18164 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+
+		int arr[][] = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+
+				if (arr[i][j] == 0) {
+					arr[i][j] = 9999999;
+				} else if (i == j) {
+					arr[i][j] = 0;
+				}
+			}
+		}
+
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (arr[i][j] < 9999999 && arr[i][j] > 0) {
+					arr[i][j] = 1;
+				} else {
+					arr[i][j] = 0;
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
