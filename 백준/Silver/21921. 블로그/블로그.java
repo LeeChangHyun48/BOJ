6a90a16c60778d5c09de53838df20a1a17685876
이창현 <116571873+LeeChangHyun48@users.noreply.github.com>
2023-09-21 20:06:34 +0900

    [Silver III] Title: 블로그, Time: 372 ms, Memory: 36076 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+
+		int arr[] = new int[N];
+		int arr_sum[] = new int[N + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 1; i <= N; i++) {
+			arr_sum[i] = arr_sum[i - 1] + arr[i - 1];
+		}
+
+		int max = 0;
+		for (int i = X; i <= N; i++) {
+			max = Math.max(max, arr_sum[i] - arr_sum[i - X]);
+		}
+
+		if (max == 0) {
+			System.out.println("SAD");
+		} else {
+			System.out.println(max);
+
+			int cnt = 0;
+			for (int i = X; i <= N; i++) {
+				if (max == arr_sum[i] - arr_sum[i - X]) {
+					cnt += 1;
+				}
+			}
+			System.out.println(cnt);
+		}
+
+	}
+
+}
